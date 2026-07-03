d4744703f84ce43ef934089c709475e6d57d2625
cngwenya727-debug <cngwenya727@gmail.com>
2026-07-03 05:18:33 -0700

    Add files via upload

---
+++ login.py
@@ -0,0 +1,14 @@
+email = ("cngwenya9912")
+password =  ("bella1234")
+verify_email = input("Enter your email : ")
+if verify_email == email:
+    print("Email verified.")
+else:
+    print("Invalid email.")
+
+verify_password = input("Enter your password :")
+if verify_password == password :
+    print("Password verified.")
+else:
+    print("Invalid password.")
+   
